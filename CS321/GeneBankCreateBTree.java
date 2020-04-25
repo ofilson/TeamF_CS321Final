a941f55272ca457d4f5bc35c1f5146a501b31285
ofilson <SuperScorer911@gmail.com>
2020-04-25 15:52:59 -0600

    Implement Sequence Scanner

--- CS321/GeneBankCreateBTree.java
+++ CS321/GeneBankCreateBTree.java
@@ -1,8 +1,11 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * @GeneBankCreateBTree:creates a BTree from a given GeneBank file.
  * 
- * @authors Michael Kinsy,   ,    ,   ,
+ * @authors Michael Kinsy, Oscar Filson,    ,   ,
  *
  */
 public class GeneBankCreateBTree {
@@ -23,32 +26,54 @@ public class GeneBankCreateBTree {
 		degree = Integer.valueOf(input_degree);
 		File file = new File(gbkFileName);
 		seq_length = Integer.valueOf(input_seq_length);
-	}
-	
-	private class BTree{
-		/**@InstanceVariables: for BTree*/
-		private int degree;
-		/**@Constructor: for BTree*/
-		private BTree(int degree) {
-			this.degree = degree;
+
+		ArrayList<String> seqList = new ArrayList<String>(); //Holds all sequences (sometimes there are more than one in a gbk file.)
+
+		try {
+			Scanner fileScan = new Scanner(file);
+			while(fileScan.hasNextLine()){
+				String line = fileScan.nextLine();
+				if(line.contains("ORIGIN")){ //Finds sequence beginning
+					StringBuilder seq = new StringBuilder();
+					line = fileScan.nextLine();
+					while(!line.contains("//")){ //Stops at //
+						Scanner lineScan = new Scanner(line);
+
+						while(lineScan.hasNext()){
+							if(lineScan.hasNextInt()){	// This removes line numbers at beginning of the line
+								lineScan.next();		
+							}							
+							String val = lineScan.next(); //Grabs next 8 characters
+							if(!val.contains("N") && !val.contains("n")){ //If 8 char sequence doens't contain n
+								seq.append(val); //Add to sequence
+							} else { //If it does contain an n
+								while(val.contains("n")){ //gets rid of all lowercase n's
+									String part1 = val.substring(0, val.indexOf("n"));
+									String part2 = val.substring(val.indexOf("n") + 1, val.length());
+									val = part1 + part2;
+								}
+								while(val.contains("N")){ //gets rid of all uppercast N's
+									String part1 = val.substring(0, val.indexOf("N"));
+									String part2 = val.substring(val.indexOf("N") + 1, val.length());
+									val = part1 + part2;
+								}
+								seq.append(val); //Adds remainder of 8 char sequence that isn't an n to sequence.
+							}
+						}
+						line = fileScan.nextLine();
+					}
+					seqList.add(seq.toString()); //Sequence added to list of sequences.
+					//System.out.println(seq.toString()); //Print for debugging pruposes
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
 		}
-	}
-	private class BTreeNode{
-		/**@InstanceVariables: for BTreeNodes*/
-		private String[] keys;
-		private int min_degree,  num_keys;
-		private BTreeNode[] Children;
-		private boolean leaf;
 		
-		/**@Constructor: for BTreeNode*/
-		private BTreeNode(int min_degree, boolean leaf) {
-			this.min_degree = min_degree;
-			this.leaf = leaf;
-			this.keys = new String[2* min_degree-1];
-			this.Children = new BTreeNode[2* min_degree];
-			this.num_keys = 0;
-		}
+
+
 	}
+
 }
 
 
